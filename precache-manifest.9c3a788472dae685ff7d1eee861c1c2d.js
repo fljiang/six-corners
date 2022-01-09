9c29f07a8da289d7fc6b7fb7e26cb38fcb3536a7
Felicia Jiang <felicia.jiang@esentire.com>
2022-01-08 22:24:59 -0500

    Updates

---
+++ precache-manifest.9c3a788472dae685ff7d1eee861c1c2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7eb09c0bf0af83e3d882",
+    "url": "/six-corners/static/css/main.3aa121f9.chunk.css"
+  },
+  {
+    "revision": "7eb09c0bf0af83e3d882",
+    "url": "/six-corners/static/js/main.14f9f5e8.chunk.js"
+  },
+  {
+    "revision": "6e8641f22f63cc4fce9f",
+    "url": "/six-corners/static/js/runtime~main.90be1644.js"
+  },
+  {
+    "revision": "a71741c43d6a6c0959b7",
+    "url": "/six-corners/static/js/2.9ece9259.chunk.js"
+  },
+  {
+    "revision": "f8fd2cad7e95cc1f808783943295962e",
+    "url": "/six-corners/index.html"
+  }
+];
